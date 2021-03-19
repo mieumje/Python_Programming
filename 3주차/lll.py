93a24186dadbc33faf10c29516b2381176f285b2
mieumje <66072832+mieumje@users.noreply.github.com>
2021-03-19 20:09:34 +0900

    scf
    문제2

--- 3주차/lll.py
+++ 3주차/lll.py
@@ -11,24 +11,20 @@ for i in range(t):
     a[i][1] = m
 
 x = 0
+
+
 while x < t:
     cnt = 0
-    if a[x][0] + a[x][1] < 12 or a[x][0] < 5:
-        cnt = 0
-    elif a[x][1] == 0:
-        cnt = a[x][0]//12
-    elif a[x][0] == 5 and a[x][1] >= 7:
-        cnt = a[x][0]//5
-    elif a[x][0] // 5 == a[x][1] // 7:
+    if a[x][0] // 5 <= a[x][1] // 7:
         cnt = a[x][0] // 5
-    elif a[x][0] // 5 < a[x][1] // 7:
-        while a[x][0] < 5:
-            cnt += a[x][1] // 7
-            a[x][0] = a[x][0] - 5 * a[x][1] // 7
-            a[x][1] = a[x][1] % 7
-            if a[x][0] + a[x][1] < 12:
-                break
-            else:
-                cnt += a[x][0] // 5
+    elif a[x][0] // 5 > a[x][1] // 7:
+        cnt += a[x][1] // 7
+        a[x][0] = a[x][0] - 5 * a[x][1] // 7
+        a[x][1] = a[x][1] % 7
+        if a[x][0] + a[x][1] >= 12:
+            a[x][0] = a[x][0] - (12 - a[x][1])
+            cnt += 1
+            cnt += a[x][0] // 12
+
     print(cnt)
     x += 1
