7db8bd3d7209154fe803ffdccd4eaa5ef54ee134
mieumje <66072832+mieumje@users.noreply.github.com>
2021-03-19 13:24:49 +0900

    3주차
    문자열 관련 연산자, 함수
    
    ord() : 특정한 한 문자를 아스키 코드 값으로 변환해 주는 함수
    chr() : 아스키 코드 값을 문자로 변환해 주는 함수(10, 16진수 사용 가능)
    16진수는 hex()

---
+++ 3주차/ex01.py
@@ -0,0 +1,35 @@
+# in 연산자
+s = 'banana'
+k = 'a' in s
+print(k)
+
+b = 'nan' in s
+print(b)
+
+if 'an' in s:
+    print('있다\n')
+else:
+    print('없다\n')
+
+
+# 문자열 비교 연산자, ==, >, <
+print(s == 'banana')
+if s == 'banana':
+    print("s는 바나나 맞음")
+else:
+    print("s는 바나나 아님")
+print(f"s > apple : {s > 'apple'}")
+print(f"s < apple : {s < 'apple'}")
+print(f"Hi > Hii : {'Hi' > 'Hii'}")
+print(f"Hi > Hi : {'Hi' > 'HI'}")
+print(f"hi == HI : {'hi' > 'HI'}\n")
+
+print(f"문자 H의 ASCII 코드 값 : {ord('H')}")
+print(f"ASCII 코드 값 72 : {chr(72)}")
+
+
+x = 'H'
+
+print("chr(ord('H')+ord('a')-ord('A'))")
+print(f"문자 {x}의 소문자 =>", end=" ")
+print(chr(ord('H')+ord('a')-ord('A')))
