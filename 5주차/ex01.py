3c9b34027bcd58daa0d096f592e6ec5b4491c6e3
mieumje <66072832+mieumje@users.noreply.github.com>
2021-04-02 13:09:20 +0900

    5주차
    (1) 여러 값(원소-element, 항목-item 이라 불리움)이 순서대로 나열된 것
    
    (2)  [ ] (대괄호, braket) 으로 감싸서 표현
    
          [10,20,30,40]
    
          [ 'Avengers', 'I spy', 'parasite', '1917' ]
    
    (3) 원소는 서로 다른 타입으로 나열될 수도 있다.
    
          [ 'banana', 32.7, 6, 0.34 ]
    
    (4) 리스트 안에 리스트가 원소로 들어갈 수 있다. - 중첩(nested) 리스트
    
          [ 1, 5, 8, [12, 14] ]
    
    (5) 비어있는 리스트를 만들 수도 있다.
    
          []
    
    (6) 상기의 리스트를 변수에 대입할 수 있고, 읽어 낼 수 있다.

---
+++ 5주차/ex01.py
@@ -0,0 +1,13 @@
+num = [10, 20, 30, 40]
+movie = ['Avengers', 'I spy']
+datatype = ['banana', 32.7, 6, 0.34]
+ndata = [1, 5, 8, [12, 13]]
+nl = []
+
+like = movie
+print(num)
+print(movie)
+print(datatype)
+print(num, ndata)
+print(nl)
+print(like)
