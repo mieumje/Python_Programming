cb5bb6e5a786afa26359f1247dee803c38464281
mieumje <66072832+mieumje@users.noreply.github.com>
2021-04-07 00:10:02 +0900

    당근마켓 애플워치 검색 결과 추출
    상위 6개만 추출
    pagination 없이 모두 추출하는 방법은?

---
+++ 5주차/5-3/ex.py
@@ -0,0 +1,39 @@
+import requests
+from bs4 import BeautifulSoup
+import csv
+
+
+item = "애플워치"
+URL = f"https://www.daangn.com/search/{item}"
+
+
+result = requests.get(URL)
+soup = BeautifulSoup(result.text, 'html.parser')
+
+section = soup.find("div", {"class": "articles-wrap"})
+
+extract_card = section.find_all(
+    "article", {"class": "flea-market-article flat-card"})
+
+title = []
+region = []
+
+
+for x in extract_card:
+    a = x.find("span", {"class": "article-title"}).string.strip()
+    b = x.find("p", {"class": "article-region-name"}).string.strip()
+    img = x.find("img")["src"]
+    c = a, b, img
+    title.append(c)
+
+
+def save_csv(item):
+    file = open("ex.csv", mode="w", encoding="utf-8")
+    writer = csv.writer(file)
+    writer.writerow(["title", "location", "image-link"])
+    for i in item:
+        writer.writerow(i)
+    return
+
+
+save_csv(title)
